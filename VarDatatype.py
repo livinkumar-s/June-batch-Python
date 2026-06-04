f903a9b3143b00255f235aa50bf1cc51a8b2b908
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-04 11:05:54 +0530

    Datatypes, Type casting and Variables

---
+++ VarDatatype.py
@@ -0,0 +1,31 @@
+# a1=10
+# print(a1)
+
+# a=10
+# a=6
+# print(a)
+
+b=50
+c=5.01
+# print(type(b))
+# print(type(c))
+# d="I'm Livinkumar"
+# d='I said "Hi"'
+
+# print(d)
+
+# print(5+1.1) # addition
+# print("Hello"+"world"+"123") #concatination
+# print(45+"54") #error
+
+# personName="Vijay"
+# age=52
+
+# He is Vijay. He is 52
+
+# print("He is "+personName+". He is "+str(age))
+
+# e=False 
+
+# print(type(e))
+
