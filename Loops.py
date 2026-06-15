2be2fad261fa3060ec9266823915e11a57ae0d86
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-15 11:51:50 +0530

    Loops - For, While

---
+++ Loops.py
@@ -0,0 +1,90 @@
+# x=0
+
+# while x<5:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("--------------------")
+#     x-=1
+
+#x -> 0,1,2,3,4,5
+# No of execution -->1,2,3,4,5
+
+# For loop 
+
+# str1="Virat"
+
+# a=range(33,63,7)
+# print(list(a))
+# print(str1[5])
+# print(len(str1))
+
+# for i in a: # 100
+#     print(i)
+#     print("----------------------")
+
+# for i in range(1,101):
+#     print(i)
+#     if i>=50:
+#         continue
+#     print("-------")
+
+# numbers: 1-49
+# No of iteration: 100
+
+# i=1
+
+# while i<=100:
+#     if i==50:
+#         i+=1 
+#         continue
+#     print(i)
+#     i+=1
+
+# 1-100 (except 50)
+# no of iteration --> 100
+
+a="Kohli"
+
+# for i in a:
+#     print(i)
+
+# print(a[0]
+
+# i=0
+
+# while i<len(a): #i<5
+#     print(a[i])
+#     i+=1
+
+# for i in range(1,101):
+#     print(i)
+
+# a=1
+# while a<=100:
+#     print(a)
+#     a+=1
+
+# secret_number=55 #1,100,1000,200
+
+# while True: 
+#     inp = int(input("Enter the number: "))
+#     if inp==secret_number:
+#         print("You won!")
+#         break
+#     else:
+#         print("Try again...!")
+
+# 100 to 1
+
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+
+# i = 100
+ 
+# while i >= 1:
+#     print(i)
+#     i -= 1
+ 
