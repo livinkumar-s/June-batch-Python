23d64aec410207a62e4d3ac49ac9a0b67dea78a3
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-19 12:14:45 +0530

    Data Structures - List, Tuple

---
+++ Datastructures.py
@@ -0,0 +1,81 @@
+# a="Hello world"
+
+# print(a[4])
+# print(a[4:5]) #"o"
+
+# print(a[4:])
+# print(a[:4])
+# print(a[2:7:2]) # 2,4,6
+# print(a[1:9:2])
+# print(a[::-1])
+
+# List 
+
+# list1=[1,2,3.47,"Hi","Hello",False,1]
+# print(len(list1))
+# print(list1[4])
+# print(list1[4:5]) #4
+# list1.append(4)
+# list1.append(5)
+# list1.insert(0,11)
+# list1.extend([1,2,3])
+
+# list1.append("Hello")
+# list1.extend("Hello")
+# list1=[1,2,3.47,"Hi","Hello",False,1]
+# list1.reverse()
+# print(list1)
+
+# list1.remove(1)
+# list1.remove(1)
+# list1.pop(3)
+# a=list1.pop()
+
+# list1[2]+=4
+
+# print(list1.index(77))
+# print(list1.count(1))
+# print(a)
+# print(len(list1))
+
+# lis1=[2,3,4,5,34,2,2,4,3,4,3]
+# lis1.sort(reverse=True)
+# print(lis1)
+
+# list1=[True,2,3,4,1]
+# list1.remove(1)
+# print(list1)
+
+# numbers=[
+#     "pine apple",
+#     "orange",
+#     "apple",
+#     1,
+#     3,
+#     0
+# ]
+
+# print("apple">1)
+
+# numbers.sort()
+
+# print(numbers)
+
+# print(numbers[-1][-1][-1][-1])
+
+# Tuple 
+
+# t1=(1,2,3,4,3,2,1)
+# t1[3]=400
+# print(t1.index(1))
+
+# name,age,job=("Ken",44,"FED")
+
+# print(name)
+
+# name=a[0]
+# age=a[1]
+# job=a[2]
+
+# a=1,2,3,4,5
+# print(a)
