d61d6e917ba81f3b89870e99898b28b3d96c547c
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-22 11:47:15 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,63 @@
+# def printSteps():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("------------------")
+
+# printSteps(3)
+# printSteps()
+# printSteps()
+
+# def addNum(a,b):
+#     print(a)
+#     print(b)
+#     print("I am adding these 2 numbers...")
+#     print(a+b)
+
+# addNum(4,4)
+# addNum(3,3)
+# addNum("Hello","Hi")
+# addNum(99,100)
+
+# def printValue(a,b,c=10):
+#     print(a*b+c)
+
+# printValue(b=10,a=2)
+
+# def printMul(*a): #tuple
+#     # print(nums)
+#     print(a)
+    
+
+# printMul(4,2,32,34,34)
+
+# def fetchDetail(**person): #Dict
+#     print(person)
+
+
+# fetchDetail(name="Ken",age=43,isMarried=True,spouse="Quen")
+
+# def sayHello():
+#     print("Hello1")
+#     print("Hello2")
+#     print("Hello3")
+#     return 
+#     print("Hello4")
+#     print("Hello5")
+
+# a=sayHello() 
+# print(a)
+
+# def sayMul(a,b):
+#     return a*b 
+
+# print(sayMul(2,sayMul(3,sayMul(4,sayMul(1,7)))))
+
+# def printMul(a,b):
+#     print(a*b)
+
+# print(printMul(4,4))
+
+# print("Have")
+# num=input("Enter the number: ")
+# print(num)
